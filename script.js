461fb587a5b154f4573f4cbfb7d180c6017454ad
민균 <>
2023-04-06 02:32:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+function App() {
+  const [counter, setCounter] = React.useState(0);
+  const onClick = () => {
+    setCounter(counter + 1)
+  }
+  return (
+    <div>
+      <h3>Total clicks: {counter}</h3>
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
+}
+
+root.render(
+  <App />
+)
